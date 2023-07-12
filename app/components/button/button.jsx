e20e274e17536a5c303d8b53796b8e1acd71737a
Jatin singh <jatinsingh10001@gmail.com>
2023-07-13 00:06:02 +0530

    initial commit

---
+++ app/components/button/button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+import styles from "./button.module.css"
+
+const button = ({url,title}) => {
+  return (
+    <Link href={url}>
+        <button className={styles.container}>{title}</button>
+    </Link>
+  )
+}
+
+export default button
---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ["images.pexels.com",
+        "educationinprogress.eu",
+        "inspirationfeed.com",
+
+      ],
+      },
+}
+
+module.exports = nextConfig
