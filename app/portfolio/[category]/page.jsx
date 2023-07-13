51b73a8fc2ff429a6c688391911a148e13fbb341
Jatin singh <jatinsingh10001@gmail.com>
2023-07-13 11:28:56 +0530

    design portfolio and category

--- app/portfolio/[category]/page.jsx
+++ app/portfolio/[category]/page.jsx
@@ -1,8 +1,63 @@
 import React from 'react'
+import styles from "./category.module.css"
+import Button from '@/app/components/button/button'
+import Image from 'next/image'
 
-const Category = () => {
+const Category = ({params}) => {
+  // console.log({params})
+  // const ids= params;
   return (
-    <div>Category</div>
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button title="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image 
+          className={styles.img}
+          src=""
+          alt="category image"
+          fill={true}
+          />
+        </div>
+
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test2</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button title="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image 
+          className={styles.img}
+          src=""
+          alt="category image"
+          fill={true}
+          />
+        </div>
+
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test3</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button title="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image 
+          className={styles.img}
+          src=""
+          alt="category image"
+          fill={true}
+          />
+        </div>
+
+      </div>
+    </div>
   )
 }
 
